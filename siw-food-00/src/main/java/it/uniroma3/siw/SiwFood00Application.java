2e49eb8e36e46fb499e6920a0c198dc37d40aab7
LAPTOP-GPD32DD8\utente <vincenzomilillo2002@gmail.com>
2024-05-03 17:01:13 +0200

    commit del proggetto vuoto

---
+++ siw-food-00/src/main/java/it/uniroma3/siw/SiwFood00Application.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwFood00Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwFood00Application.class, args);
+	}
+
+}
