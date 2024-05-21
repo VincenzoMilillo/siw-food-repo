231fb337a6ee123d65574439d90cd69316a15b67
LAPTOP-GPD32DD8\utente <vincenzomilillo2002@gmail.com>
2024-05-21 22:35:08 +0200

    modifiche al form di login

--- siw-food-00/src/main/java/it/uniroma3/siw/controller/AuthenticationController.java
+++ siw-food-00/src/main/java/it/uniroma3/siw/controller/AuthenticationController.java
@@ -1,12 +1,17 @@
 package it.uniroma3.siw.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
+import it.uniroma3.siw.model.Credentials;
+import it.uniroma3.siw.model.User;
 import it.uniroma3.siw.service.CredentialsService;
 import it.uniroma3.siw.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 
 @Controller
@@ -23,4 +28,10 @@ public class AuthenticationController {
 		return "formLogin.html";
 	}
     
+    @GetMapping(value = "/register") 
+	public String showRegisterForm (Model model) {
+		model.addAttribute("user", new User());
+		model.addAttribute("credentials", new Credentials());
+		return "formRegisterUser";
+	}
 }
